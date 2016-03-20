db01a71e043b925d0abc93c60be733d667237f8c
Masataro Asai <guicho2.71828@gmail.com>
2016-03-20 17:02:17 +0900

    move tiebreaking_open_list class definition to header files

--- src/search/open_lists/tiebreaking_open_list.cc
+++ src/search/open_lists/tiebreaking_open_list.cc
@@ -15,43 +15,6 @@
 
 using namespace std;
 
-
-template<class Entry>
-class TieBreakingOpenList : public OpenList<Entry> {
-    using Bucket = deque<Entry>;
-
-    map<const vector<int>, Bucket> buckets;
-    int size;
-
-    vector<ScalarEvaluator *> evaluators;
-    /*
-      If allow_unsafe_pruning is true, we ignore (don't insert) states
-      which the first evaluator considers a dead end, even if it is
-      not a safe heuristic.
-    */
-    bool allow_unsafe_pruning;
-
-    int dimension() const;
-
-protected:
-    virtual void do_insertion(EvaluationContext &eval_context,
-                              const Entry &entry) override;
-
-public:
-    explicit TieBreakingOpenList(const Options &opts);
-    virtual ~TieBreakingOpenList() override = default;
-
-    virtual Entry remove_min(vector<int> *key = nullptr) override;
-    virtual bool empty() const override;
-    virtual void clear() override;
-    virtual void get_involved_heuristics(set<Heuristic *> &hset) override;
-    virtual bool is_dead_end(
-        EvaluationContext &eval_context) const override;
-    virtual bool is_reliable_dead_end(
-        EvaluationContext &eval_context) const override;
-};
-
-
 template<class Entry>
 TieBreakingOpenList<Entry>::TieBreakingOpenList(const Options &opts)
     : OpenList<Entry>(opts.get<bool>("pref_only")),
--- src/search/open_lists/tiebreaking_open_list.h
+++ src/search/open_lists/tiebreaking_open_list.h
@@ -5,6 +5,45 @@
 
 #include "../option_parser_util.h"
 
+#include <deque>
+#include <map>
+#include <vector>
+
+template<class Entry>
+class TieBreakingOpenList : public OpenList<Entry> {
+    using Bucket = std::deque<Entry>;
+
+
+    std::vector<ScalarEvaluator *> evaluators;
+    /*
+      If allow_unsafe_pruning is true, we ignore (don't insert) states
+      which the first evaluator considers a dead end, even if it is
+      not a safe heuristic.
+    */
+    bool allow_unsafe_pruning;
+
+    int dimension() const;
+
+protected:
+    std::map<const std::vector<int>, Bucket> buckets;
+    int size;
+    virtual void do_insertion(EvaluationContext &eval_context,
+                              const Entry &entry) override;
+public:
+    explicit TieBreakingOpenList(const Options &opts);
+    virtual ~TieBreakingOpenList() override = default;
+
+    virtual Entry remove_min(std::vector<int> *key = nullptr) override;
+    virtual bool empty() const override;
+    virtual void clear() override;
+    virtual void get_involved_heuristics(std::set<Heuristic *> &hset) override;
+    virtual bool is_dead_end(
+        EvaluationContext &eval_context) const override;
+    virtual bool is_reliable_dead_end(
+        EvaluationContext &eval_context) const override;
+};
+
+
 
 class TieBreakingOpenListFactory : public OpenListFactory {
     Options options;
