064c6364a49b6683da980490c70dc762e37dde76
Masataro Asai <guicho2.71828@gmail.com>
2016-03-22 02:38:53 +0900

    test.sh: quick test script for myself

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+with-dir (){
+    mkdir -p $1
+    pushd $1
+    shift
+    echo $@
+    eval $*
+    popd
+}
+
+# CC=clang-3.5 CXX=clang-3.5
+
+# with-dir builds/debug32/ LINKER=clang-3.5 CC=clang-3.5 CXX=clang-3.5 cmake ../../src
+# with-dir builds/debug32/ LINKER=clang-3.5 CC=clang-3.5 CXX=clang-3.5 make
+
+# CC=clang CXX=clang cmake ../../src
+# CC=clang CXX=clang make
+
+./cgroup-setup.sh
+
+./build.py && ./build.py --debug && cgexec -g memory:$(whoami)/ misc/tests/run-all-code-tests
