2109c06c633ca52513cbec1d8259dc67cf9317e1
Masataro Asai <guicho2.71828@gmail.com>
2016-03-21 09:37:48 +0900

    added parent method to searchspace

--- src/search/search_space.cc
+++ src/search/search_space.cc
@@ -135,6 +135,12 @@ void SearchSpace::trace_path(const GlobalState &goal_state,
     reverse(path.begin(), path.end());
 }
 
+const GlobalState SearchSpace::parent(const GlobalState &state) const {
+    return
+        g_state_registry->lookup_state(
+            search_node_infos[state].parent_state_id);
+}
+
 void SearchSpace::dump() const {
     for (PerStateInformation<SearchNodeInfo>::const_iterator it =
              search_node_infos.begin(g_state_registry);
