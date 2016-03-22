55986993181844dd4cada8485ff1bdee45410bab
Masataro Asai <guicho2.71828@gmail.com>
2016-03-22 22:36:06 +0900

    debugging utility --- GDB is a poor man's SLIME & not that different from printf

---
+++ src/search/debug.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+
+template<typename V>
+std::ostream &operator<<(std::ostream &o, const std::vector<V> &v){
+    o << "#(";
+    bool first = true;
+    for (const V &elem : v){
+        if (first){
+            first = false;
+        }else{
+            o << " ";
+        }
+        o << elem ;
+    }
+    o << ")";
+    return o;
+}
+
+template<typename T1,typename T2>
+std::ostream &operator<<(std::ostream &o, const std::pair<T1,T2> &v){
+    o << "(" << v.first << " . " << v.second << ")";
+    return o;
+}
+
+
