f8b0c017bda29f6faa0d5187d279b0632c6daf06
Masataro Asai <guicho2.71828@gmail.com>
2017-04-05 19:10:55 +0900

    based on typed_tiebreaking

--- src/search/open_lists/fractal_open_list.h
+++ src/search/open_lists/fractal_open_list.h
@@ -1,51 +1,30 @@
+/* -*- mode : c++ -*- */
 #ifndef OPEN_LISTS_FRACTAL_OPEN_LIST_H
 #define OPEN_LISTS_FRACTAL_OPEN_LIST_H
 
 #include "open_list_factory.h"
-#include "open_list.h"
+#include "typed_tiebreaking_open_list.h"
 
 #include "../option_parser_util.h"
+#include "../utils/collections.h"
 
-#include <deque>
-#include <vector>
-#include <set>
-#include <memory>
-
-/*
-  Bucket-based implementation of an open list.
-
-  Nodes with identical heuristic value are expanded in FIFO order.
-*/
-
-
-class ScalarEvaluator;
+// #include <deque>
+// #include <map>
+// #include <unordered_map>
+// #include <vector>
 
 template<class Entry>
-class FractalOpenList : public OpenList<Entry> {
-    typedef std::deque<Entry> Bucket;
-    std::vector<Bucket> buckets;
-    mutable int lowest_bucket;
-    int size;
-
-    std::unique_ptr<OpenList<Entry>> sublist;
-    ScalarEvaluator *evaluator;
-
-protected:
-    virtual void do_insertion(EvaluationContext &eval_context,
-                              const Entry &entry) override;
+class FractalOpenList : public TypedTiebreakingOpenList<Entry> {
 
 public:
+private:
+    int max_depth;
+protected:
+public:
     explicit FractalOpenList(const Options &opts);
-    virtual ~FractalOpenList() override = default;
+    // virtual ~FractalOpenList() override {};
 
     virtual Entry remove_min(std::vector<int> *key = nullptr) override;
-    virtual bool empty() const override;
-    virtual void clear() override;
-    virtual void get_involved_heuristics(std::set<Heuristic *> &hset) override;
-    virtual bool is_dead_end(
-        EvaluationContext &eval_context) const override;
-    virtual bool is_reliable_dead_end(
-        EvaluationContext &eval_context) const override;
 };
 
 class FractalOpenListFactory : public OpenListFactory {
