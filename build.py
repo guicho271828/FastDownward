05d1575e10e815363477016007e0776095e6d197
Masataro Asai <guicho2.71828@gmail.com>
2017-04-06 10:22:43 +0900

    struggle

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ CONFIGS = {
     "release32": ["-DCMAKE_BUILD_TYPE=Release"],
     "debug32":   ["-DCMAKE_BUILD_TYPE=Debug"],
     "release64": ["-DCMAKE_BUILD_TYPE=Release", "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"],
-    "debug64":   ["-DCMAKE_BUILD_TYPE=Debug",   "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"],
+    "debug64":   ["-DCMAKE_BUILD_TYPE=Debug",   "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"], # "--trace",
 }
 DEFAULT_DEBUG_CONFIG = "debug64"
 DEFAULT_CONFIG = "release64"
--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -46,7 +46,6 @@ set(CORE_SOURCES
         task_proxy.cc
         task_tools.cc
         variable_order_finder.cc
-        backward.cc
 
         open_lists/alternation_open_list.cc
         open_lists/weighted_alternation_open_list.cc
