91a755cab0a1d67f35b0d27fb94a6b89f23a242e
Masataro Asai <guicho2.71828@gmail.com>
2017-11-13 18:40:09 +0900

    fails to build opcount on Ubuntu17.04; possibly changes in the default gcc setting

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -393,17 +393,17 @@ fast_downward_plugin(
     DEPENDS LP_SOLVER
 )
 
-fast_downward_plugin(
-    NAME OPERATOR_COUNTING
-    HELP "Plugin containing the code for operator counting heuristics"
-    SOURCES
-        operator_counting/constraint_generator.cc
-        operator_counting/lm_cut_constraints.cc
-        operator_counting/operator_counting_heuristic.cc
-        operator_counting/pho_constraints.cc
-        operator_counting/state_equation_constraints.cc
-    DEPENDS LP_SOLVER LANDMARK_CUT_HEURISTIC PDBS
-)
+# fast_downward_plugin(
+#     NAME OPERATOR_COUNTING
+#     HELP "Plugin containing the code for operator counting heuristics"
+#     SOURCES
+#         operator_counting/constraint_generator.cc
+#         operator_counting/lm_cut_constraints.cc
+#         operator_counting/operator_counting_heuristic.cc
+#         operator_counting/pho_constraints.cc
+#         operator_counting/state_equation_constraints.cc
+#     DEPENDS LP_SOLVER LANDMARK_CUT_HEURISTIC PDBS
+# )
 
 fast_downward_plugin(
     NAME PDBS
